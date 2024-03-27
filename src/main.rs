a80a21f97f14614306d6b119330e1d3ea300858e
Jason Brown <jajabro@google.com>
2024-03-27 17:51:46 +0000

    still messing about and nothing working

--- src/main.rs
+++ src/main.rs
@@ -10,22 +10,24 @@ use sophia_turtle::parser::turtle::TurtleParser;
 use sophia_api::prelude::TripleParser;
 
 
-const FILE_NAME: &str = "/Users/jajabro/development/knowledge_graph/organizational-change-tracker/rdf/ontology/bank-organization.ttl";
+const FILE_NAME: &str = "/usr/local/google/home/jajabro/Development/knowledge-graph/organizational-change-tracker/rdf/ontology/bank-organization.ttl";
 
 fn main() {
     let f_str = fs::read_to_string(FILE_NAME);
+    let file = File::open(FILE_NAME);
+    let mut b_str = BufReader::new(file.unwrap());
 
 
     let p = TurtleParser {
         base: Some(Iri::new_unchecked("http://localhost/ex".to_string())),
     };
 
-
-
     let g = FastGraph::new();
 
-    // let c = TripleParser::parse(&p, f_bufreader);
-    let c = p.parse_str(&f_str.unwrap());
+    let c = TripleParser::parse(&p, &mut b_str);
+
+    let _c = p.parse_str(&f_str.unwrap());
+
 }
 
 
