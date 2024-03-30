9af77f9c8f69ed5172b88d0f9a2fbfda6224b2b6
Jason Brown <jajabro@icloud.com>
2024-03-30 09:17:50 +0100

    first graph built

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
 use std::fs;
-use sophia_turtle::parser::turtle::parse_bufread;
-// use rio_turtle::{TurtleParser, TurtleError};
-// use rio_api::parser::TriplesParser;
 use std::io::BufReader;
 use std::fs::File;
 use sophia_iri::Iri;
 use sophia_inmem::graph::FastGraph;
 use sophia_turtle::parser::turtle::TurtleParser;
 use sophia_api::prelude::TripleParser;
+use sophia_api::source::TripleSource;
+use sophia_api::graph::MutableGraph;
 
 
-const FILE_NAME: &str = "rdf/ontology/bank-organization.ttl";
+const FILE_NAME: &str = "/Users/jajabro/development/knowledge_graph/organizational-change-tracker/rdf/ontology/bank-organization.ttl";
 
 fn main() {
     let f_str = fs::read_to_string(FILE_NAME);
@@ -22,12 +21,19 @@ fn main() {
         base: Some(Iri::new_unchecked("http://localhost/ex".to_string())),
     };
 
-    let g = FastGraph::new();
+    let mut g = FastGraph::new();
 
-    let c = TripleParser::parse(&p, &mut b_str);
+    let mut c = TripleParser::parse(&p, &mut b_str);
 
-    let _c = p.parse_str(&f_str.unwrap());
+    let _d = p.parse_str(&f_str.unwrap());
+    
+    let _ = TripleSource::for_each_triple(&mut c, |t| {
+        let _ =MutableGraph::insert_triple(&mut g, t);
+    });
 
+    sophia_api::graph::Graph::objects(&g).for_each(|p| {
+        println!("{:?}", p);
+    });
 }
 
 
